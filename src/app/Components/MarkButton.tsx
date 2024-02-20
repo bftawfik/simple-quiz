ff4c6f1945ecd293bff3d49d9180b1b1ea03e372
nour <nourelturky9@gmail.com>
2024-02-20 13:50:47 +0200

    create ui for home page

---
+++ src/app/Components/MarkButton.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from "react";
+interface ButtonProps {
+  onClick: (value: number) => void;
+  values: number[];
+}
+
+const MarkButton = ({ onClick, values }: ButtonProps) => {
+  return (
+    <div className="flex flex-wrap gap-2">
+      {values.map(value => (
+        <button
+          key={value}
+          onClick={() => onClick(value)}
+          className="bg-[#4CB087] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          {value}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default MarkButton;
+
