ac5357c99c7669376729fac0084ef31151f28611
Minatallah Hisham <mennahishamh@gmail.com>
2024-02-20 14:36:41 +0200

    3 create results page

---
+++ src/app/_components/resultSection/resultSection.tsx
@@ -0,0 +1,17 @@
+interface ResultSectionProps {
+  category: string;
+  question: string;
+  score: number;
+}
+
+const ResultSection = ({ category, question, score }: ResultSectionProps) => {
+  return (
+    <div className="bg-opacity-50 bg-emerald-500 m-2 p-2 rounded-lg w-3/6">
+      <h1 className="text-base font-bold">{category}</h1>
+      <p className="text-white text-base font-normal">{question}</p>
+      <p>Score: {score}/10</p>
+    </div>
+  );
+};
+
+export default ResultSection;
