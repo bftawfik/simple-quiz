b6ea8f845fab32194a4da925f5f2d87d8abf4ef3
nour <nourelturky9@gmail.com>
2024-02-21 17:17:23 +0200

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import QuestionsNumber from './_components/questionsNumber/QuestionsNumber';
+import HomePage from './_components/homePage/HomePage';
 
 export default function Home() {
-  return <QuestionsNumber />;
+  return <HomePage />;
 }
