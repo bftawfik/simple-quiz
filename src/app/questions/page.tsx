354a511da71a09138190f411ae8af670485da5d2
nour <nourelturky9@gmail.com>
2024-02-21 13:49:33 +0200

    create start page

---
+++ src/app/questions/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import QuestionsNumber from '../_components/questionNumber/QuestionsNumber'
+
+export default function page() {
+  return (
+    <QuestionsNumber />
+  )
+}
