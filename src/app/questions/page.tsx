a1f8048256f906f1d9c92b4846f40a933a11f182
nour <nourelturky9@gmail.com>
2024-02-25 16:28:43 +0200

    get data from store

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store'
 import QuestionsSlider from './_components/questionSlide/QuestionSlide';
-import data from '@/../../data.json';
 
 const Home = () => {
+  const data = useSelector((state: RootState) => state.questions.value)
   return <QuestionsSlider data={data} />;
 };
 
